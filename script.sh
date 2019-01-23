8ad9405440d8cb630c2360347a5914dfde6d5c22
Christos Ioannidis <christos.ioannidis@stud.uni-bamberg.de>
2019-01-23 15:05:48 +0100

    fix circle ci config (arm32v7->arm32v6)

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
-# 
-# make ARCHITECTURES=arm32v6
-# make ARCHITECTURES=arm64v8
-# make ARCHITECTURES=amd64
-# 
-# make ARCHITECTURES=arm32v6 push
+ 
+ make ARCHITECTURES=arm32v6
+ make ARCHITECTURES=arm64v8
+ make ARCHITECTURES=amd64
+ 
+ make ARCHITECTURES=arm32v6 push
 make ARCHITECTURES=arm64v8 push
 make ARCHITECTURES=amd64 push
 
