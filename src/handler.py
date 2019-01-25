b44019922c1f660ec927979f0b07a9d850b75488
Christos Ioannidis <christos.ioannidis@stud.uni-bamberg.de>
2019-01-25 12:14:56 +0100

    use env var

--- src/handler.py
+++ src/handler.py
@@ -13,7 +13,7 @@ def handle(req):
     converter = InfluxDBConverter(jreq['measurements_name'])
     influxdb_json=converter.convert(jreq)
     #print("Result of Conversion:\n" + str(influxdb_json))
-    influx = influxdb.InfluxDBClient('127.0.0.1', 8086, 'root', 'root', 'sensiot')
+    influx = influxdb.InfluxDBClient(os.environ['db_ip'], 8086, 'root', 'root', 'sensiot')
     influx.write_points(influxdb_json.get())
     return 'OK'
 
@@ -52,4 +52,4 @@ class InfluxDBFormat:
     def __str__(self):
         return json.dumps(self.data, indent=2)
 
-#handle('{"sensor_id": 3, "type": "SensorMock", "measurements": [{"name": "temperature", "value": -23.0, "unit": "\u00b0C"}, {"name": "humidity", "value": -42.0, "unit": "\u00b0C"}], "hostname": "1ec3f4c24036", "device_id": "RPiDev1", "building": "L9", "room": "Living Room", "measurements_name":"Temp & Humidity", "timestamp": 1548262139}')
+handle('{"sensor_id": 3, "type": "SensorMock", "measurements": [{"name": "temperature", "value": -23.0, "unit": "\u00b0C"}, {"name": "humidity", "value": -42.0, "unit": "\u00b0C"}], "hostname": "1ec3f4c24036", "device_id": "RPiDev1", "building": "L9", "room": "Living Room", "measurements_name":"Temp & Humidity", "timestamp": 1548262139}')
